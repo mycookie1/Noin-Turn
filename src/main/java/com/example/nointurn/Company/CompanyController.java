cc534e6b96277e5694dde9f87354ca650be90ad8
Cookie <youbellgun31@gmail.com>
2023-08-24 22:38:06 +0900

    [feat] swagger 설정

--- src/main/java/com/example/nointurn/Company/CompanyController.java
+++ src/main/java/com/example/nointurn/Company/CompanyController.java
@@ -3,6 +3,12 @@ package com.example.nointurn.Company;
 import com.example.nointurn.Company.model.PostCompanyReq;
 import com.example.nointurn.User.model.GetCompanyRes;
 import com.example.nointurn.User.model.GetUserRes;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -20,6 +26,15 @@ public class CompanyController {
      * @param companyIdx
      * @return
      */
+    @Tag(name = "회사 디테일 API")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "요청에 성공하였습니다.")
+    })
+
+    @Operation(summary = "회사 디테일 API", description = "회사 디테일 API")
+    @Parameters({
+            @Parameter(name = "companyIdx", description = "기업 인덱스 정보", example = "1")
+    })
     @ResponseBody
     @GetMapping("/detail/{companyIdx}")
     public GetCompanyRes getCompanyDetail(@PathVariable Long companyIdx) {
@@ -31,6 +46,15 @@ public class CompanyController {
      * @param userIdx
      * @return
      */
+    @Tag(name = "연락하기 API")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "요청에 성공하였습니다.")
+    })
+
+    @Operation(summary = "연락하기 API", description = "유저의 전화번호 + 기업이 유저에게 보낸 요청리스트에 추가")
+    @Parameters({
+            @Parameter(name = "userIdx", description = "유저 인덱스 정보", example = "1")
+    })
     @ResponseBody
     @GetMapping("/phone/{companyIdx}/{userIdx}")
     public String getPhone(@PathVariable Long companyIdx, @PathVariable Long userIdx) {
@@ -42,6 +66,15 @@ public class CompanyController {
      * @param companyIdx
      * @return
      */
+    @Tag(name = "기업에 지원한 지원자 리스트 API")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "요청에 성공하였습니다.")
+    })
+
+    @Operation(summary = "기업에 지원한 지원자 리스트 API", description = "기업에 지원한 지원자 리스트 API")
+    @Parameters({
+            @Parameter(name = "companyIdx", description = "기업 인덱스 정보", example = "1")
+    })
     @ResponseBody
     @GetMapping("/apply/{companyIdx}")
     public List<GetUserRes> getApplyRes(@PathVariable Long companyIdx) {
@@ -53,6 +86,15 @@ public class CompanyController {
      * @param companyIdx
      * @return
      */
+    @Tag(name = "기업이 요청보낸 지원자 리스트 API")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "요청에 성공하였습니다.")
+    })
+
+    @Operation(summary = "기업이 요청보낸 지원자 리스트 API", description = "기업이 요청보낸 지원자 리스트 API")
+    @Parameters({
+            @Parameter(name = "companyIdx", description = "기업 인덱스 정보", example = "1")
+    })
     @ResponseBody
     @GetMapping("/contact/{companyIdx}")
     public List<GetUserRes> getContact(@PathVariable Long companyIdx) {
@@ -64,6 +106,15 @@ public class CompanyController {
      * @param companyIdx
      * @return
      */
+    @Tag(name = "기업의 요청과 유저의 경력이 일치하는 리스트 API")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "요청에 성공하였습니다.")
+    })
+
+    @Operation(summary = "기업의 요청과 유저의 경력이 일치하는 리스트 API", description = "기업의 요청과 유저의 경력이 일치하는 리스트 API")
+    @Parameters({
+            @Parameter(name = "companyIdx", description = "기업 인덱스 정보", example = "1")
+    })
     @ResponseBody
     @GetMapping("/match/{companyIdx}")
     public List<GetUserRes> getUser(@PathVariable Long companyIdx) {
--- src/main/java/com/example/nointurn/User/UserController.java
+++ src/main/java/com/example/nointurn/User/UserController.java
@@ -3,6 +3,12 @@ package com.example.nointurn.User;
 
 import com.example.nointurn.User.entity.User;
 import com.example.nointurn.User.model.*;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -22,6 +28,15 @@ public class UserController {
      * @param userIdx
      * @return
      */
+    @Tag(name = "유저의 개인정보를 보는 API ")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "요청에 성공하였습니다.")
+    })
+
+    @Operation(summary = "유저의 개인정보 요청 API", description = "유저의 개인정보 요청 API")
+    @Parameters({
+            @Parameter(name = "userIdx", description = "유저의 정보", example = "1")
+    })
     @ResponseBody
     @GetMapping("/{userIdx}")
     public GetUserRes getUser(@PathVariable(required = true) Long userIdx) {
@@ -33,6 +48,15 @@ public class UserController {
      * @param userIdx
      * @return
      */
+    @Tag(name = "유저의 능력과 맞는 기업의 리스트")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "요청에 성공하였습니다.")
+    })
+
+    @Operation(summary = "유저와 능력과 기업의 요청이 맞는 기업의 리스트를 보냄", description = "유저와 능력과 기업의 요청이 맞는 기업의 리스트를 보냄")
+    @Parameters({
+            @Parameter(name = "userIdx", description = "유저의 정보", example = "1")
+    })
     @ResponseBody
     @GetMapping("find/{userIdx}")
     public List<GetFindJobRes> getJobs(@PathVariable(required = true) Long userIdx) {
@@ -44,6 +68,15 @@ public class UserController {
      * @param userIdx
      * @return
      */
+    @Tag(name = "기업이 제안한 일자리 리스트")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "요청에 성공하였습니다.")
+    })
+
+    @Operation(summary = "기업들이 유저에게 보낸 요청 리스트", description = "기업들이 유저에게 보낸 요청 리스트")
+    @Parameters({
+            @Parameter(name = "userIdx", description = "유저의 정보", example = "1")
+    })
     @ResponseBody
     @GetMapping("offer/{userIdx}")
     public List<GetOfferJobRes> getOffer(@PathVariable(required = true) Long userIdx) {
@@ -55,6 +88,15 @@ public class UserController {
      * @param companyIdx
      * @return
      */
+    @Tag(name = "기업의 디테일 정보 API")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "요청에 성공하였습니다.")
+    })
+
+    @Operation(summary = "기업의 디테일 정보 API", description = "기업의 디테일 정보 API")
+    @Parameters({
+            @Parameter(name = "companyIdx", description = "기업의 인덱스 정보", example = "1")
+    })
     @ResponseBody
     @GetMapping("company/{companyIdx}")
     public GetCompanyRes getCompanyRes(@PathVariable(required = true) Long companyIdx) {
@@ -66,6 +108,15 @@ public class UserController {
      * @param userIdx
      * @return
      */
+    @Tag(name = "유저가 지원한 기업리스트")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "요청에 성공하였습니다.")
+    })
+
+    @Operation(summary = "유저가 지원한 기업리스트 API", description = "유저가 지원한 기업리스트 API")
+    @Parameters({
+            @Parameter(name = "userIdx", description = "유저 인덱스 정보", example = "1")
+    })
     @ResponseBody
     @GetMapping("apply/{userIdx}")
     public List<GetApplyJobRes> getApplyJobRes(@PathVariable(required = true) Long userIdx) {
@@ -78,6 +129,16 @@ public class UserController {
      * @param companyIdx
      * @return
      */
+    @Tag(name = "유저가 기업에 지원하는 API")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "요청에 성공하였습니다.")
+    })
+
+    @Operation(summary = "유저가 기업에 지원하는 API", description = "유저가 기업에 지원하는 API")
+    @Parameters({
+            @Parameter(name = "userIdx", description = "유저 인덱스 정보", example = "1"),
+            @Parameter(name = "companyIdx", description = "기업 인덱스 정보", example = "1")
+    })
     @ResponseBody
     @PostMapping("apply/{userIdx}/{companyIdx}")
     public Long postApplyJob(@PathVariable Long userIdx,@PathVariable Long companyIdx) {
